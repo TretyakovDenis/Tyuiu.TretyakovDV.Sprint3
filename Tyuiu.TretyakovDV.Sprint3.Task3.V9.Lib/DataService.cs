36819d03dcc4fc9b5f00080e1a4a248c85aa531a
Tyuiu.TretyakovDV@outlook.com <trev1793@gmail.com>
2023-10-22 18:44:56 +0500

    task3

---
+++ Tyuiu.TretyakovDV.Sprint3.Task3.V9.Lib/DataService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+
+namespace Tyuiu.TretyakovDV.Sprint3.Task3.V9.Lib
+{
+    public class DataService : ISprint3Task3V9
+    {
+        public string ReplaceNumOnChar(string value, char item)
+        {
+            foreach (char c in value)
+            {
+                if (c == '1' | c == '2' | c == '3' | c == '4' | c == '5' | c == '6' | c == '7' | c == '8' | c == '9' | c == '0')
+                {
+                    value = value.Replace(c, item);
+                }
+            }
+            
+            return value;
+        }
+    }
+}
+//if (c == '1' | c == '2' | c == '3' | c == '4' | c == '5' | c == '6' | c == '7' | c == '8' | c == '9' | c == '0')
---
+++ Tyuiu.TretyakovDV.Sprint3.Task3.V9/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.TretyakovDV.Sprint3.Task3.V9.Lib;
+
+namespace Tyuiu.TretyakovDV.Sprint3.Task3.V9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Третьяков Д.В. | ПКТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #9                                                              *");
+            Console.WriteLine("* Выполнил: Третьяков Денис Викторович | ПКТб-23-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Используя цикл foreach заменить цифры на букву w в строке:              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите строку");
+            string value = Console.ReadLine();
+            Console.WriteLine("Введите букву");
+            char item = Convert.ToChar(Console.ReadLine());
+            value = ds.ReplaceNumOnChar(value, item);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(value);
+            Console.ReadKey();
+
+
+        }
+    }
+}
