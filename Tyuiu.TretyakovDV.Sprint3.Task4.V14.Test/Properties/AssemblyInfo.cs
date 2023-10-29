aae0b21e52e41a8497eec20787b7b4ced0113e7e
Tyuiu.TretyakovDV@outlook.com <trev1793@gmail.com>
2023-10-29 17:11:51 +0500

    task4

---
+++ Tyuiu.TretyakovDV.Sprint3.Task4.V14.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.TretyakovDV.Sprint3.Task4.V14.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.TretyakovDV.Sprint3.Task4.V14.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2c28f5fa-52c5-46bd-a20b-c545010d6d70")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.TretyakovDV.Sprint3.Task4.V14/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.TretyakovDV.Sprint3.Task4.V14.Lib;
+
+namespace Tyuiu.TretyakovDV.Sprint3.Task4.V14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #3 | Выполнил: Третьяков Д.В. | ПКТб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #4                                                              *");
+            Console.WriteLine("* Вариант #14                                                             *");
+            Console.WriteLine("* Выполнил: Третьяков Денис Викторович | ПКТб-23-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение     *");
+            Console.WriteLine("* функции y=cos(x)/sin(x). При х = 0 прервать цикл.                       *");
+            Console.WriteLine("* Полученные значения суммировать.                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*ИСХОДНЫЕ ДАННЫЕ:                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("Введите начальную координату х");
+            int startValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите конечную координату х");
+            int stopValue = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*РЕЗУЛЬТАТ:                                                               *");
+            Console.WriteLine("***************************************************************************");
+            double res = ds.Calculate(startValue, stopValue);
+            Console.WriteLine(res);
+            Console.ReadKey();
+
+
+        }
+    }
+}
