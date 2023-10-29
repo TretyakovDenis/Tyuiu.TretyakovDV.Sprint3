e4817d20f1d353b3caa81af1c0ce2afd22ae4ac4
Tyuiu.TretyakovDV@outlook.com <trev1793@gmail.com>
2023-10-29 19:58:22 +0500

    task7

---
+++ Tyuiu.TretyakovDV.Sprint3.Task7.V7.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.TretyakovDV.Sprint3.Task7.V7.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.TretyakovDV.Sprint3.Task7.V7.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9da22df8-cec4-4cd8-babb-f2d5f5acc5ff")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
